168ab4c04156ff651548023b76562f2e44ca3590
muzammi961 <muzammil2332005@gmail.com>
2025-08-10 14:43:49 +0530

    tenth commit

--- src/components/order/ordersection.jsx
+++ src/components/order/ordersection.jsx
@@ -69,7 +69,7 @@ let totalprice=orderproduct.reduce((acc,item)=>acc + item.product.offer_price*it
 
     return (
   <div 
-    style={{backgroundImage:`url(${bgimage})`}} 
+    style={{ backgroundImage: `url(${bgimage})` }}
     className="bg-fixed bg-center bg-cover bg-no-repeat min-h-screen"
   >
     <div className="max-w-4xl mx-auto p-4">
